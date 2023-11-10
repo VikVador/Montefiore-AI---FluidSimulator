383994eb0d64af186408c96aa582cbf549016ab2
Victor <VMangeleer@student.uliege.be>
2023-11-10 15:14:37 +0100

    Only work with conda

--- setup.py
+++ setup.py
@@ -6,13 +6,6 @@ VERSION          = '1.0.0'
 DESCRIPTION      = 'Montefiore AI - Fluid Simulator'
 LONG_DESCRIPTION = 'Montefiore AI - Fluid Simulator : A library to make fluid simulations solving Burger (1D), Navier-Stokes (2D), and many more using JAXCFD !'
 
-# All dependencies
-requirement_path = "requirements.txt"
-install_requires = []
-if os.path.isfile(requirement_path):
-    with open(requirement_path) as f:
-        install_requires = f.read().splitlines()
-
 # Seting up everything
 setup(
       name             = "maifs",
@@ -23,5 +16,4 @@ setup(
       description      = DESCRIPTION,
       long_description = LONG_DESCRIPTION,
       packages         = ["maifs"],
-      install_requires = install_requires,
 )
